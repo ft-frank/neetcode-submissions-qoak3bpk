9a87fd23ade0caed91bb9fbbc1e21571a4b50961
Frank Tran <166266231+ft-frank@users.noreply.github.com>
2026-07-22 14:48:53 +1000

    Add: hand-of-straights - submission-15

---
+++ Data Structures & Algorithms/hand-of-straights/submission-15.py
@@ -0,0 +1,35 @@
+"""
+Count all cards
+
+Then loop through each card in hand.
+
+Check 
+
+"""
+
+from collections import Counter
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0: #initial check to see if we can make n-integer straights
+            return False
+        c = Counter(hand) #count how many copies of each card we have
+
+    
+        for v in c: #iterate through each card
+            start = v #this is our start value, we think will start the straight. in greedy consecutive, we need this.
+
+            while c[start-1] > 0: # move the start back until we it diconnects, so we can start the straight there
+                start -= 1
+            
+            while start <= v: #while the start is less than the value we started with
+                while c[start]: #while we still have copies to use at the start of our straight
+                    for j in range(start, start + groupSize): #check the straight, if doesn't exist, then return False
+                        if not c[j]:
+                            return False
+                        c[j] -= 1
+                start += 1 #move the start up, until you get to v. 
+
+        return True        
+                
+                    
