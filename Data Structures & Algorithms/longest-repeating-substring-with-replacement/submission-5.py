330f1378c862c8acbf863f769eaad88b1581f93b
Frank Tran <166266231+ft-frank@users.noreply.github.com>
2026-08-04 14:41:39 +1000

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.py
@@ -0,0 +1,43 @@
+"""
+We can use a sliding window approach, that attempts to create the largest valid window, and then once can't extend any longer, records length
+and then resets window.
+
+
+For example 2:
+
+
+"""
+
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = r = 0
+        res = 1
+        seen = {}
+        top_frequency = 0
+
+        while r < len(s):
+            char = s[r]
+            seen[char] = seen.get(char, 0) + 1 #we have seen this char
+            top_frequency = max(top_frequency, seen[char])  #we find max top_frequency
+
+            #r - l + 1 - top_frequncy must be max k, any more than k we have to remove characters
+
+            while r-l+1-top_frequency > k:
+                remove = s[l]
+                seen[remove] -=1
+                top_frequency = max(top_frequency, seen[remove])
+                #how to adjust top_frequency?
+                l+=1
+            res = max(r-l+1, res)
+            r += 1
+
+        return res
+
+
+
+
+
+
+    
+            
