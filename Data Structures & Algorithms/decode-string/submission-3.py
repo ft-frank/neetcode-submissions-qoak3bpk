e893ad11992718112cddbd21b90549ab6feb3846
Frank Tran <166266231+ft-frank@users.noreply.github.com>
2026-08-13 14:44:34 +1000

    Add: decode-string - submission-3

---
+++ Data Structures & Algorithms/decode-string/submission-3.py
@@ -0,0 +1,52 @@
+"""
+We use a stack data structure.
+This is recursion problem, I have identified this because there are nested structures witin the s.
+
+
+
+"""
+
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        
+        res = []
+
+        def recurse(index, number):
+            cur = []
+            while index < len(s) and s[index] != ']':
+                if s[index].isdigit():
+                    r = index
+                    while r < len(s) and s[r].isdigit():
+                        r+=1
+                    recursion = recurse(r+1, int("".join(s[index:r])))  #skip the bracket following the integer
+                    cur.append(recursion[0]) 
+                    index = recursion[1] + 1
+                else:
+                    cur.append(s[index])
+                    index += 1
+            joined = "".join(cur)
+            return (joined * number, index)
+
+        i = 0
+
+        while i < len(s):
+            recursion = recurse(i, 1)
+            res.append(recursion[0])
+            i= recursion[1]
+            i += 1
+
+        return "".join(res)
+        
+
+
+
+            
+
+
+
+
+
+
+
+
